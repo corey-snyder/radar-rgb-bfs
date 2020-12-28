6444a9d6ff711863f0958b6cedc128cdd7cc8e05
Corey Snyder <cesnyde2@illinois.edu>
2020-12-27 20:40:56 -0600

    Adding radar-supervised pcp code and example code in rpca_test.ipynb

--- pcp.py
+++ pcp.py
@@ -91,6 +91,74 @@ def pcp(M, delta=1e-6, mu=None, maxiter=500, verbose=False, missing_data=True,
         logging.warn("convergence not reached in pcp")
     return L, S, (u, s, v)
 
+def radar_pcp(M, M_F, delta=1e-6, mu=None, maxiter=500, verbose=False, missing_data=True,
+        svd_method="approximate", **svd_args):
+    # Check the SVD method.
+    allowed_methods = ["approximate", "exact", "sparse"]
+    if svd_method not in allowed_methods:
+        raise ValueError("'svd_method' must be one of: {0}"
+                         .format(allowed_methods))
+
+    # Check for missing data.
+    shape = M.shape
+    if missing_data:
+        missing = ~(np.isfinite(M))
+        if np.any(missing):
+            M = np.array(M)
+            M[missing] = 0.0
+    else:
+        missing = np.zeros_like(M, dtype=bool)
+        if not np.all(np.isfinite(M)):
+            logging.warn("The matrix has non-finite entries. "
+                         "SVD will probably fail.")
+
+    # Initialize the tuning parameters.
+    lam = 1.0 / np.sqrt(np.max(shape))
+    if mu is None:
+        mu = 0.25 * np.prod(shape) / np.sum(np.abs(M))
+        if verbose:
+            print("mu = {0}".format(mu))
+
+    # Convergence criterion.
+    norm = np.sum(M ** 2)
+
+    # Iterate.
+    i = 0
+    rank = np.min(shape)
+    S = np.zeros(shape)
+    Y = np.zeros(shape)
+    while i < max(maxiter, 1):
+        # SVD step.
+        strt = time.time()
+        u, s, v = _svd(svd_method, M - S + Y / mu, rank+1, 1./mu, **svd_args)
+        svd_time = time.time() - strt
+
+        s = shrink(s, 1./mu)
+        rank = np.sum(s > 0.0)
+        u, s, v = u[:, :rank], s[:rank], v[:rank, :]
+        L = np.dot(u, np.dot(np.diag(s), v))
+
+        # Shrinkage step.
+        S = shrink(M - L + Y / mu, lam / mu * M_F)
+
+        # Lagrange step.
+        step = M - L - S
+        step[missing] = 0.0
+        Y += mu * step
+
+        # Check for convergence.
+        err = np.sqrt(np.sum(step ** 2) / norm)
+        if verbose:
+            print(("Iteration {0}: error={1:.3e}, rank={2:d}, nnz={3:d}, "
+                   "time={4:.3e}")
+                  .format(i, err, np.sum(s > 0), np.sum(S > 0), svd_time))
+        if err < delta:
+            break
+        i += 1
+
+    if i >= maxiter:
+        logging.warn("convergence not reached in pcp")
+    return L, S, (u, s, v)
 
 def shrink(M, tau):
     sgn = np.sign(M)
