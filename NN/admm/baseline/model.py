22d05a8a2a4c927f4af7eb89866007109bf764f7
Spencer Markowitz <samarko2@illinois.edu>
2021-04-18 13:58:21 -0500

    admm running on local

--- NN/admm/baseline/model.py
+++ NN/admm/baseline/model.py
@@ -33,7 +33,7 @@ class AdmmLayer(nn.Module):
 
         self.threshold = nn.Threshold(0,0)
 
-    # parallel processing (k+1 is only dependent on k)
+    # parallel processing (k+1 is only dependent on k except lagrange multiplier)
     def forward(self, input):
         """
         :param input:
@@ -42,96 +42,56 @@ class AdmmLayer(nn.Module):
 
         (D, L, S, Y) = input
 
-        L1 = self.p1(L)
-        L2 = self.p2(L)
+        # do S and L in parallel
+        G1 = self.p2(D + Y/self.mu1 - self.p1(L))
+        G2 = self.p4(D + Y/self.mu2 - self.p3(S))
 
-        Y1 = self.mu1 * Y
-        Y2 = self.mu2 * Y
-
-        S3 = self.p3 * S
-        S4 = self.p4 * S
+        # create k+1 sparse component
+        S_out = self.threshold(G1 - self.lambda1)
 
         # create k+1 low rank component
-        D_S4_Y2 = D - S4 - Y2
-        D_S4_Y2 = torch.reshape(D_S4_Y2, (-1, self.im_height * self.im_width)).T
-        (u, s, v) = torch.svd(D_S4_Y2)
-        s = s - self.lambda1
+        G2 = torch.reshape(G2, (-1, self.im_height * self.im_width)).T
+        (u, s, v) = torch.svd(G2)
+        s = s - self.lambda2
         s = self.threshold(s)
         L_stacked = torch.mm(torch.mm(u, torch.diag(s)), v.t())
         L_out = L_stacked.T.reshape(-1, 1, self.im_height, self.im_width)
 
-        # create k+1 sparse component
-        D_L1_Y1 = D - L1 + Y1
-        S_out = self.threshold(D_L1_Y1-self.lambda2)
-
         # create k+1 lagrangian step
-        Y_out = Y + self.mu3 * (D-L2-S3)
-
-        return (D, L_out, S_out, Y_out)
-
-    # sequential processing (k+1 is dependent on other k+1 terms)
-    def forward(self, input):
-        """
-        :param input:
-        :return:
-        """
-
-        (D, L, S, Y) = input
-
-        Y1 = self.mu1 * Y
-        Y2 = self.mu2 * Y
-
-        # create k+1 low rank component
-        D_S4_Y2 = D - S4 - Y2
-        D_S4_Y2 = torch.reshape(D_S4_Y2,(-1,self.im_height*self.im_width)).T
-
-        (u, s, v) = torch.svd(D_S4_Y2)
-        s = s - self.lambda1
-        s = self.threshold(s)
-        L_stacked = torch.mm(torch.mm(u, torch.diag(s)), v.t())
-        L_out = L_stacked.T.reshape(-1, 1, self.im_height, self.im_width)
-
-        # create k+1 sparse component
-        L1 = self.p2(L_out)
-        D_L1_Y1 = D - L1 + Y1
-        S_out = self.threshold(D_L1_Y1 - self.lambda2)
-
-        # create k+1 lagrangian step
-        L2 = self.p2(L)
-        S3 = self.p3(S)
-        Y_out = Y + self.mu3 * (D-L2-S3)
+        Y_out = Y + self.mu3 * (D-L_out-S_out)
 
         return D, L_out, S_out, Y_out
 
-    class ADMMNet(nn.Module):
-        def __init__(self, shape, n_layers):
-            super().__init__()
-            self.n_channels = shape[1]
-            self.im_height = shape[2]
-            self.im_width = shape[3]
 
-            self.layers = nn.ModuleList()
-            for layer_idx in range(n_layers):
-                if layer_idx < 3:
-                    kernel_size = 5
-                else:
-                    kernel_size = 3
-                padding = int(np.floor(kernel_size / 2))
-                self.layers.append(AdmmLayer(self.im_height, self.im_width, self.n_channels, kernel_size, padding=padding))
+class AdmmNet(nn.Module):
+    def __init__(self, shape, n_layers):
+        super().__init__()
+        self.n_channels = shape[1]
+        self.im_height = shape[2]
+        self.im_width = shape[3]
 
-        def forward(self, D):
-            """
-            :param input: tuple of D
-            :return: tuple of L, S
-            """
-            S = torch.zeros_like(D)
-            L = torch.zeros_like(D)
-            Y = torch.zeros_like(D)
+        self.layers = nn.ModuleList()
+        for layer_idx in range(n_layers):
+            if layer_idx < 3:
+                kernel_size = 5
+            else:
+                kernel_size = 3
+            padding = int(np.floor(kernel_size / 2))
+            self.layers.append(AdmmLayer(self.im_height, self.im_width, self.n_channels, kernel_size, padding=padding))
 
-            components = (D, L, S, Y)
+    def forward(self, D):
+        """
+        :param input: tuple of D
+        :return: tuple of L, S
+        """
+        S = torch.zeros_like(D)
+        L = torch.zeros_like(D)
+        Y = torch.zeros_like(D)
 
-            for layer in self.layers:
-                components = layer(components)
-            (D, L, S, Y) = components
+        components = (D, L, S, Y)
 
-            return torch.cat((L, S), 1)
+        for layer in self.layers:
+            components = layer(components)
+        (D, L, S, Y) = components
+
+        return torch.cat((L, S), 1)
--- NN/admm/baseline/train.py
+++ NN/admm/baseline/train.py
@@ -2,116 +2,41 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from model import AdmmNet
 from torch.utils.tensorboard import SummaryWriter
-from tensorboard_helper import plot_classes_preds
 from skimage.transform import rescale
 import argparse
 import yaml
 from datetime import datetime
 import shutil
+import sys
+sys.path.append('../../baseline')
+from NN.baseline.tensorboard_helper import plot_classes_preds
+from NN.baseline.train import load_data, random_crop, pad_mat
 
 import matplotlib.pyplot as plt
-
-
-def load_data(path, n_frames = 30, rescale_factor = 1., S=None, L=None, print_flag=True):
-
-    D = np.load(path + '/D.npy')
-    if L is None:
-        L = np.load(path + '/L_pcp.npy')
-        if print_flag: print('Using ' + path + '/L_pcp.npy')
-    else:
-        if print_flag: print('Using ' + L)
-        L = np.load(L)
-
-    if S is None:
-        S = np.load(path + '/S_pcp.npy')
-        if print_flag: print('Using ' + path + '/S_pcp.npy')
-    else:
-        if print_flag: print('Using ' + S)
-        S = np.load(S)
-
-
-    # Add channel dimension, new shape = [n_frames,1,720,1280] not including downsampling
-    D = D[:n_frames, None,:,:]
-    L = L[:n_frames, None,:,:]
-    S = S[:n_frames, None,:,:]
-
-    # Downsample with anti-aliasing filter
-    if rescale_factor != 1:
-        D = rescale(D,(1,1,rescale_factor,rescale_factor),anti_aliasing=True)
-        L = rescale(L,(1,1,rescale_factor,rescale_factor),anti_aliasing=True)
-        S = rescale(S,(1,1,rescale_factor,rescale_factor),anti_aliasing=True)
-
-    return torch.from_numpy(D).float(), torch.from_numpy(L).float(), torch.from_numpy(S).float()
-
-
-def random_crop(D, target, patch_shape):
-    (full_height, full_width) = D.shape[-2:]
-    (patch_height, patch_width) = patch_shape
-
-    # assuming coordinates belong to center of the patch so we have to be side_len/2 away from boundary
-    height_range = (patch_height//2,full_height-patch_height//2)
-    width_range = (patch_width//2,full_width-patch_width//2)
-
-    rand_height = np.random.choice(np.arange(height_range[0],height_range[1]))
-    rand_width = np.random.choice(np.arange(width_range[0],width_range[1]))
-
-    # Extracting Patch
-    D_patch = D[:,:,rand_height-patch_height//2: rand_height+patch_height//2,
-                    rand_width-patch_width//2: rand_width+patch_width//2]
-    target_patch = target[:,:,rand_height-patch_height//2:rand_height+patch_height//2,
-                              rand_width-patch_width//2:rand_width+patch_width//2]
-
-    return D_patch, target_patch
-
-
-def pad_mat(og_input,patch_shape, step_shape):
-    (_, _, og_height, og_width) = og_input.shape
-    (height_step, width_step) = step_shape
-    (patch_height, patch_width) = patch_shape
-
-    # determine height padding amount
-    new_height = patch_height
-    while new_height < og_height:
-        new_height += height_step
-
-    # determine width padding amount
-    new_width = patch_width
-    while new_width < og_width:
-        new_width += width_step
-
-    height_pad_len = new_height-og_height
-    width_pad_len = new_width-og_width
-
-    pad = torch.nn.ZeroPad2d((0,width_pad_len,0, height_pad_len))
-
-    padded_input = pad(og_input)
-
-    return padded_input
 
 
 def infer_full_image(full_input, network, patch_shape, step_shape, device):
     # https://discuss.pytorch.org/t/how-to-split-tensors-with-overlap-and-then-reconstruct-the-original-tensor/70261
     torch.cuda.empty_cache()
     (height_step, width_step) = step_shape
-    (patch_height,patch_width) = np.array(patch_shape[-2:])
-    (batch_size,channels,og_height, og_width) = full_input.shape
+    (patch_height, patch_width) = np.array(patch_shape[-2:])
+    (batch_size, channels, og_height, og_width) = full_input.shape
 
     # determine padding (need to pad so that the size of the input is matches the patch/step size)
-    padded_input = pad_mat(full_input,(patch_height,patch_width),(height_step, width_step))
+    padded_input = pad_mat(full_input, (patch_height, patch_width), (height_step, width_step))
 
     patches = padded_input.unfold(2, size=int(patch_height), step=int(height_step)).unfold(3, size=int(patch_width), step=int(width_step))
     patches_out = torch.zeros((batch_size, 2, patches.shape[2], patches.shape[3], patch_height, patch_width)).to(device)
     for ii in range(patches.shape[2]):
         for jj in range(patches.shape[3]):
-            patch_input = patches[:,:,ii,jj].to(device)
-            L_patch_input = torch.zeros_like(patch_input).to(device)
-            S_patch_input = torch.zeros_like(patch_input).to(device)
-            patches_out[:,:,ii,jj] = network(patch_input, L_patch_input, S_patch_input)
+            patch_input = patches[:, :, ii, jj].to(device)
+            patches_out[:, :, ii, jj] = network(patch_input)
 
     # fold data
     # reshape output to match F.fold input
-    patches_out = patches_out.contiguous().view(batch_size, 2, -1, patch_height*patch_width)
+    patches_out = patches_out.contiguous().view(batch_size, 2, -1, patch_height * patch_width)
     # print(patches_out.shape)  # [B, C, nb_patches_all, kernel_size*kernel_size]
     patches_out = patches_out.permute(0, 1, 3, 2)
     # print(patches_out.shape)  # [B, C, kernel_size*kernel_size, nb_patches_all]
@@ -125,23 +50,21 @@ def infer_full_image(full_input, network, patch_shape, step_shape, device):
 
     # fold ones
     ones_tensor = torch.ones_like(patches_out).to(device)
-    
-    # memory saver
-    del L_patch_input, S_patch_input, patches_out
-    torch.cuda.empty_cache()
 
+    # memory saver
+    del patches_out
+    torch.cuda.empty_cache()
 
     ones_tensor = fold(ones_tensor)
 
     # data /ones
     padded_output = padded_output / ones_tensor
-    full_output = padded_output[:,:,:og_height,:og_width].to(device)
+    full_output = padded_output[:, :, :og_height, :og_width].to(device)
 
     return full_output
 
 
 if __name__ == '__main__':
-    from model import IstaNet
 
     parser = argparse.ArgumentParser()
     parser.add_argument("-yaml", help="path of yaml file", type=str)
@@ -149,7 +72,7 @@ if __name__ == '__main__':
     yampl_path = args.yaml
 
     with open(yampl_path) as file:
-        setup_dict = yaml.load(file,Loader=yaml.FullLoader)
+        setup_dict = yaml.load(file, Loader=yaml.FullLoader)
     train_path = setup_dict['train_path'][0]
     test_path = setup_dict['test_path'][0]
     n_layers = setup_dict['n_layers'][0]
@@ -162,14 +85,13 @@ if __name__ == '__main__':
     patch_width = setup_dict['patch_width'][0]
     mask_bool = setup_dict['mask'][0]
     seed = setup_dict['seed'][0]
-    S_train_path = setup_dict['S_train'][0] 
+    S_train_path = setup_dict['S_train'][0]
     L_train_path = setup_dict['L_train'][0]
     S_test_path = setup_dict['S_test'][0]
     L_test_path = setup_dict['L_test'][0]
 
     torch.manual_seed(seed)
     np.random.seed(seed)
-
 
     # check if CUDA is available
     if try_gpu:
@@ -184,35 +106,34 @@ if __name__ == '__main__':
         print('CUDA is available!  Training on GPU ...')
 
     # load Data (not dataset object since no train/test split)
-    D_train_full,L_train_full_target,S_train_full_target = load_data(train_path,rescale_factor=downsample_rate, S=S_train_path, L=L_train_path, mask_bool=mask_bool)
-    target_train_full = torch.cat((L_train_full_target,S_train_full_target),1)  # concatenate the L and S in the channel dimension
+    D_train_full, L_train_full_target, S_train_full_target = load_data(train_path, rescale_factor=downsample_rate, S=S_train_path, L=L_train_path)
+    target_train_full = torch.cat((L_train_full_target, S_train_full_target), 1)  # concatenate the L and S in the channel dimension
     D_train_full.to(device)
 
-    D_test_full, L_test_full_target, S_test_full_target = load_data(test_path, rescale_factor=downsample_rate, S=S_test_path, L=L_test_path, mask_bool=mask_bool)
+    D_test_full, L_test_full_target, S_test_full_target = load_data(test_path, rescale_factor=downsample_rate, S=S_test_path, L=L_test_path)
     target_test_full = torch.cat((L_test_full_target, S_test_full_target), 1)  # concatenate the L and S in the channel dimension
     D_test_full.to(device)
 
     # Destination for tensorboard log data
     now = datetime.now()
     dt_string = now.strftime("%d-%m-%Y_%H-%M-%S")
-    log_name = 'OG_l'+str(n_layers) + '_lr_' + str(learning_rate) + '_ds_' + str(downsample_rate)
-    log_dir = '../runs/'+log_name+'__'+dt_string
+    log_name = 'OG_l' + str(n_layers) + '_lr_' + str(learning_rate) + '_ds_' + str(downsample_rate)
+    log_dir = '../runs/' + log_name + '__' + dt_string
     writer = SummaryWriter(log_dir)
     shutil.copyfile(yampl_path, log_dir + '/setup.yaml')
 
     # init model
-    data_shape = list(np.concatenate([D_train_full.shape[:2],[patch_height,patch_width]]))
-    model = IstaNet(data_shape,n_layers)
+    data_shape = list(np.concatenate([D_train_full.shape[:2], [patch_height, patch_width]]))
+    model = AdmmNet(data_shape, n_layers)
     model.to(device)
     # specify loss function (categorical cross-entropy)
     criterion = nn.MSELoss()
     # specify optimizer
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    scheduler = optim.lr_scheduler.StepLR(optimizer,gamma=schedule_multiplier,step_size=schedule_step)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, gamma=schedule_multiplier, step_size=schedule_step)
 
     # tensorboard graph
-    writer.add_graph(model,(D_train_full[:,:,:patch_height,:patch_width].to(device),D_train_full[:,:,:patch_height,:patch_width].to(device),
-                            D_train_full[:,:,:patch_height,:patch_width].to(device)))
+    writer.add_graph(model, (D_train_full[:, :, :patch_height, :patch_width].to(device)))
     # writer.close()
 
     # train
@@ -233,17 +154,16 @@ if __name__ == '__main__':
         # clear the gradients of all optimized variables
         optimizer.zero_grad()
 
-        # init L, S as zeros per PCP algorithm
-        L_train_patch = torch.zeros(data_shape)
-        S_train_patch = torch.zeros(data_shape)
+        # # init L, S as zeros per PCP algorithm
+        # L_train_patch = torch.zeros(data_shape)
+        # S_train_patch = torch.zeros(data_shape)
 
         # get random crop
-        D_train_patch, target_train_patch = random_crop(D_train_full,target_train_full,(patch_height,patch_width))
+        D_train_patch, target_train_patch = random_crop(D_train_full, target_train_full, (patch_height, patch_width))
         # send tensors to device
-        D_train_patch, L_train_patch, S_train_patch, target_train_patch = \
-            D_train_patch.to(device), L_train_patch.to(device), S_train_patch.to(device), target_train_patch.to(device)
+        D_train_patch, target_train_patch = D_train_patch.to(device), target_train_patch.to(device)
         # forward pass: compute predicted outputs by passing inputs to the model
-        output = model(D_train_patch,L_train_patch,S_train_patch)
+        output = model(D_train_patch)
         # calculate the batch loss
         loss = criterion(output, target_train_patch)
         # backward pass: compute gradient of the loss with respect to model parameters
@@ -275,27 +195,26 @@ if __name__ == '__main__':
         # writer.close()
 
         # show sample predictions
-        if epoch % 250 ==0:
+        if epoch % 250 == 0:
             # memory saver
-            del L_train_patch, S_train_patch, loss #L_flat, u, s, v
+            del loss  # L_flat, u, s, v
 
             model.eval()
-            step_shape = np.array(data_shape[-2:])//2
-            output_train_full = infer_full_image(D_train_full,model,data_shape,step_shape, device)
+            step_shape = np.array(data_shape[-2:]) // 2
+            output_train_full = infer_full_image(D_train_full, model, data_shape, step_shape, device)
 
             # compute pixel loss of entire image
             target_train_full.to(device)
-            loss = criterion(output_train_full,target_train_full.to(device))
+            loss = criterion(output_train_full, target_train_full.to(device))
             train_loss = loss.item()
             writer.add_scalar('Training Pixel loss Full',
                               loss.item(),
                               epoch)
             # writer.close()
-            #if (epoch % 1000 ==0) and (epoch <10000):
-            #    writer.add_figure('predictions vs. actuals TRAIN',
-            #              plot_classes_preds(output_train_full.cpu().detach().numpy(),L_train_full_target.cpu().numpy(),S_train_full_target.cpu().numpy()),
-            #              global_step=epoch)
-                # writer.close()
+            if (epoch % 100 ==0) and (epoch <10000):
+               writer.add_figure('predictions vs. actuals TRAIN',
+                         plot_classes_preds(output_train_full.cpu().detach().numpy(),L_train_full_target.cpu().numpy(),S_train_full_target.cpu().numpy()),
+                         global_step=epoch)
             del output_train_full, loss
 
             ######################
@@ -309,12 +228,12 @@ if __name__ == '__main__':
                               loss.item(),
                               epoch)
             # writer.close()
-            #if (epoch % 1000 ==0) and (epoch <10000):
+            # if (epoch % 1000 ==0) and (epoch <10000):
             #    writer.add_figure('predictions vs. actuals TEST',
             #                  plot_classes_preds(output_test_full.cpu().detach().numpy(), L_test_full_target.cpu().numpy(),
             #                                     S_test_full_target.cpu().numpy()),
             #                  global_step=epoch)
-                # writer.close()
+            # writer.close()
 
             print('Epoch: {} \tTraining Loss: {:.6f} \tTest Loss: {:.6f}'.format(
                 epoch, train_loss, valid_loss))
@@ -332,6 +251,5 @@ if __name__ == '__main__':
         else:
             print('Epoch: {} \tTraining Loss: {:.6f}'.format(
                 epoch, train_loss))
-
 
         scheduler.step()
