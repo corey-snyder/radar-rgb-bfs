41e5e5fde28e74e6815ba7843e824ad75b60a1d1
Spencer Markowitz <samarko2@illinois.edu>
2021-04-19 16:02:15 -0500

    radar at thresh with cosine loss

--- NN/radar/model.py
+++ NN/radar/model.py
@@ -46,7 +46,6 @@ class IstaLayer(nn.Module):
         :param L: Stacked  low rank components
         :param S: Stacked sparse components
         :return: tuple of D, L^{k+1}, S^{k+1}
-
         notation for intermediate values will follow that of the cited paper.
         For example, L convolved with P_5 will be labeled L5
         """
@@ -65,7 +64,6 @@ class IstaLayer(nn.Module):
 
         L5_D1_S3 = L5+D1+S3
         L6_D2_S4 = L6+D2+S4
-        L6_D2_S4_radar = L6_D2_S4 * R7_2dim
 
         L5_D1_S3 = torch.reshape(L5_D1_S3,(-1,self.im_height*self.im_width)).T
 
@@ -77,7 +75,7 @@ class IstaLayer(nn.Module):
         D_out = D
         L_out = L_stacked.T.reshape(-1,1,self.im_height,self.im_width)
 
-        S_out = torch.sign(L6_D2_S4_radar)*self.threshold(torch.abs(L6_D2_S4_radar)-self.lambda2)
+        S_out = torch.sign(L6_D2_S4)*self.threshold(torch.abs(L6_D2_S4)-nn.functional.relu(R7_2dim)*self.lambda2)
 
         return D_out, L_out, S_out, R
 
@@ -112,14 +110,3 @@ class IstaNet(nn.Module):
             (D,L,S,R) = components
 
             return torch.cat((L,S),1)
-
-
-
-
-
-
-
-
-
-
-
