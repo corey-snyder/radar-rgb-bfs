174417b001bad79365199bc7856fae38b95d192a
Spencer Markowitz <samarko2@illinois.edu>
2021-04-23 13:02:24 -0500

    visualization first commit

--- NN/radar/model.py
+++ NN/radar/model.py
@@ -50,7 +50,7 @@ class IstaLayer(nn.Module):
         For example, L convolved with P_5 will be labeled L5
         """
         # print(self.lambda1, self.lambda2)
-        (D,L,S,R) = input
+        (D,L,S,R,save_sparse) = input
 
         L5 = self.p5(L)
         L6 = self.p6(L)
@@ -77,7 +77,10 @@ class IstaLayer(nn.Module):
 
         S_out = torch.sign(L6_D2_S4)*self.threshold(torch.abs(L6_D2_S4)-nn.functional.relu(R7_2dim)*self.lambda2)
 
-        return D_out, L_out, S_out, R
+        if save_sparse:
+            return D_out, L_out, S_out, R, save_sparse, L6_D2_S4
+
+        return D_out, L_out, S_out, R, save_sparse
 
 
 class IstaNet(nn.Module):
@@ -99,14 +102,25 @@ class IstaNet(nn.Module):
                 self.layers.append(IstaLayer(self.im_height, self.im_width, self.n_channels, im_kernel_size=im_kernel_size,
                                    im_padding=im_padding,  radar_kernel_size = radar_kernel_size,radar_padding=radar_kernel_padding))
 
-        def forward(self, D, L, S, R):
+        def forward(self, D, L, S, R, save_sparse=False):
             """
             :param input: tuple of D,L,S
             :return: tuple of D, L, S
             """
-            components = (D,L,S,R)
-            for layer in self.layers:
-                components = layer(components)
-            (D,L,S,R) = components
+            if not save_sparse:
+                components = (D,L,S,R,save_sparse)
+                for layer in self.layers:
+                    components = layer(components)
+                (D,L,S,R,save_sparse) = components
 
-            return torch.cat((L,S),1)
+                return torch.cat((L,S),1)
+
+            else:
+                L6_D2_S4_list = []
+                components = (D, L, S, R, save_sparse)
+                for layer in self.layers:
+                    components = layer(components)
+                    L6_D2_S4_list.append(components[-1])
+                    components = components[:-1]
+                (D, L, S, R, save_sparse) = components
+                return torch.cat((L, S), 1), torch.cat(L6_D2_S4_list,1)
--- NN/radar/viz_filters.py
+++ NN/radar/viz_filters.py
@@ -5,8 +5,9 @@ import torch.optim as optim
 from model import IstaNet
 import argparse
 import yaml
-from train import load_data, infer_full_image
+from train import load_data
 import matplotlib.pyplot as plt
+from train import pad_mat
 from matplotlib import gridspec
 
 
@@ -21,21 +22,108 @@ def plot_weights(model):
     for layer in range(num_layers):
         num_weights = len(weight_names)
         for idx, weight in enumerate(weight_names):
-            plt.subplot(num_layers+2,num_weights,layer*num_weights+idx+1)
+            plt.subplot(num_layers,num_weights,layer*num_weights+idx+1)
             try: plt.imshow(getattr(model.layers[layer],weight).weight[0,0].detach().numpy())
             except: plt.imshow(getattr(model.layers[layer],weight).weight[0].detach().numpy())
             plt.xticks([])
             plt.yticks([])
             if idx == 0: plt.ylabel('Layer ' + str(layer))
             plt.title(weight)
-    net_image=plt.imread('model_pic.png')
-    plt.subplot(num_layers+1,1,num_layers+1,)
-    plt.imshow(net_image)
-    plt.xticks([])
-    plt.yticks([])
-    plt.tight_layout()
+    # net_image=plt.imread('model_pic.png')
+    # plt.subplot(num_layers+1,1,num_layers+1,)
+    # plt.imshow(net_image)
+    # plt.xticks([])
+    # plt.yticks([])
     plt.show()
 
+
+def view_sparse_progressions(full_rgb_input, full_radar_input, network, patch_shape, step_shape, device):
+    # https://discuss.pytorch.org/t/how-to-split-tensors-with-overlap-and-then-reconstruct-the-original-tensor/70261
+    torch.cuda.empty_cache()
+    (height_step, width_step) = step_shape
+    (patch_height,patch_width) = np.array(patch_shape[-2:])
+    (batch_size,channels,og_height, og_width) = full_rgb_input.shape
+
+    # determine padding (need to pad so that the size of the input is matches the patch/step size)
+    padded_rgb_input, padded_radar_input = pad_mat(full_rgb_input, full_radar_input, (patch_height,patch_width),(height_step, width_step))
+
+    patches_rgb = padded_rgb_input.unfold(2, size=int(patch_height), step=int(height_step)).unfold(3, size=int(patch_width), step=int(width_step))
+    patches_radar = padded_radar_input.unfold(2, size=int(patch_width), step=int(width_step))
+    patches_out = torch.zeros((batch_size, 2, patches_rgb.shape[2], patches_rgb.shape[3], patch_height, patch_width)).to(device)
+    sparse_out = torch.zeros((batch_size, 2, patches_rgb.shape[2], patches_rgb.shape[3], patch_height, patch_width)).to(device)
+    for ii in range(patches_rgb.shape[2]):
+        for jj in range(patches_rgb.shape[3]):
+            patch_rgb_input = patches_rgb[:,:,ii,jj].to(device)
+            patch_radar_input = patches_radar[:,:,jj].to(device)
+            L_patch_input = torch.zeros_like(patch_rgb_input).to(device)
+            S_patch_input = torch.zeros_like(patch_rgb_input).to(device)
+            patches_out[:,:,ii,jj], sparse_out[:,:,ii,jj] = network(patch_rgb_input, L_patch_input, S_patch_input, patch_radar_input,save_sparse=True)
+
+    # fold data
+    # reshape output to match F.fold input
+    patches_out = patches_out.contiguous().view(batch_size, 2, -1, patch_height*patch_width)
+    sparse_out = sparse_out.contiguous().view(batch_size, 2, -1, patch_height * patch_width)
+    # print(patches_out.shape)  # [B, C, nb_patches_all, kernel_size*kernel_size]
+    patches_out = patches_out.permute(0, 1, 3, 2)
+    sparse_out = sparse_out.permute(0, 1, 3, 2)
+    # print(patches_out.shape)  # [B, C, kernel_size*kernel_size, nb_patches_all]
+    patches_out = patches_out.contiguous().view(batch_size, 2 * patch_height * patch_width, -1)
+    sparse_out = sparse_out.contiguous().view(batch_size, 2 * patch_height * patch_width, -1)
+    # print(patches_out.shape)  # [B, C*prod(kernel_size), L] as expected by Fold
+    # https://pytorch.org/docs/stable/nn.html#torch.nn.Fold
+
+    fold = torch.nn.Fold(output_size=padded_rgb_input.shape[-2:], kernel_size=(patch_height, patch_width), stride=(height_step, width_step))
+    padded_output = fold(patches_out)
+    padded_sparse_checkpoints = fold(sparse_out)
+
+    # print(output.shape)  # [B, C, H, W]
+
+
+    # fold ones
+    ones_tensor = torch.ones_like(patches_out).to(device)
+
+    # memory saver
+    del L_patch_input, S_patch_input, patch_radar_input, patches_out, sparse_out
+    torch.cuda.empty_cache()
+
+    ones_tensor = fold(ones_tensor)
+
+    # data /ones
+    padded_output = padded_output / ones_tensor
+    padded_sparse_checkpoints = padded_sparse_checkpoints / ones_tensor
+
+    full_output = padded_output[:,:,:og_height,:og_width].to(device)
+    full_sparse_checkpoints = padded_sparse_checkpoints[:,:,:og_height,:og_width].to(device)
+    return full_output, full_sparse_checkpoints
+
+
+def plot_data(full_rgb_input, full_radar_input, model, patch_shape, step_shape, frame, device):
+    output, sparse_checkpoints = view_sparse_progressions(full_rgb_input, full_radar_input, model, patch_shape, step_shape, 'cpu')
+    output, sparse_checkpoints = output.detach().numpy(), sparse_checkpoints.detach().numpy()
+    fig = plt.figure(constrained_layout=True)
+    spec2 = gridspec.GridSpec(ncols=2, nrows=len(model.layers)+1, figure=fig)
+    for idx, layer in enumerate(model.layers):
+        ax = fig.add_subplot(spec2[idx, 0])
+        plt.imshow(sparse_checkpoints[frame,idx])
+        plt.xticks([])
+        plt.yticks([])
+        plt.ylabel('Layer '+str(idx))
+        plt.title('Before the Thresh')
+        ax = fig.add_subplot(spec2[idx, 1])
+        plt.plot((layer.lambda2*nn.functional.relu(layer.p7(full_radar_input))[frame,0]).detach().numpy())
+        plt.xticks([])
+        plt.title('Thresholds')
+    ax = fig.add_subplot(spec2[-1, 0])
+    plt.imshow(output[frame, 1])
+    plt.xticks([])
+    plt.yticks([])
+    plt.title('Final Sparse Output')
+    ax = fig.add_subplot(spec2[-1, 1])
+    plt.plot(full_radar_input[frame,0].detach().numpy())
+    plt.title('Radar Input')
+    plt.xticks([])
+
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -67,8 +155,6 @@ if __name__ == '__main__':
     model.load_state_dict(torch.load(net_path))
     model.eval()
 
-    # output = infer_full_image(D[:30], R[:30], model, data_shape, [30,30], 'cpu')
-    # plt.imshow(np.abs(output[10,1].detach().numpy()))
-    # plt.show()
+    plot_data(D[:30], R[:30], model, data_shape, [30, 30], 10, 'cpu')
 
     plot_weights(model)
