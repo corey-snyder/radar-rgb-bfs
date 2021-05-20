02f1b0b71c40660eb12f27ba24d15f8464c4212a
Spencer Markowitz <samarko2@illinois.edu>
2021-05-20 15:05:42 -0400

    updated evaluate targets

--- evaluate_targets.py
+++ evaluate_targets.py
@@ -4,39 +4,48 @@ import argparse
 import yaml
 import os
 from skimage.io import imread
-from evaluate import compute_metrics, display_results
+from skimage.transform import rescale
+from NN.utils.evaluation_utils import *
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-S", help="path of Sparse numpy file", type=str)
-    parser.add_argument("-ds",help="downsample rate per dimension", type=float)
+    parser.add_argument("-rescale",help="downsample rate per dimension", type=float)
     parser.add_argument("-GT", help="path of dir of GT", type=str)
     args = parser.parse_args()
     s_path = args.S
-    downsample_rate = args.ds
+    rescale_factor = args.rescale
     gt_path = args.GT
 
     # Load Data
-    S = np.load(s_path)   
-    # Compute F-scores
+    S = np.load(s_path)
 
+    # Compute F-scores
     thresholds = [i * 0.05 for i in range(20)]
     filenames = os.listdir(gt_path)
     gt_images = []
     for ii in range(len(filenames)):
         gt_images.append(imread(os.path.join(gt_path, 'rgb_%i.png'%ii)))
-    if downsample_rate !=1:
-        gt_images = [gt_images[ii][::int(1/downsample_rate),::int(1/downsample_rate)] for ii in range(len(gt_images))]
-        S = S[:,::int(1/downsample_rate),::int(1/downsample_rate)]
-    output_len = 30 #S.shape[0]
-    gt_images = gt_images[:output_len]
-    pred_images = [np.array(np.abs(S[ii])*255,dtype=np.uint8) for ii in range(output_len)]  # max abs val of float image is 1
-    
-    print('Computing full metrics...')
-    results = compute_metrics(gt_images, pred_images, thresholds, False)
-    display_results(results)
 
-    print('\n\nComputing subsampled metrics...')
-    results = compute_metrics(gt_images, pred_images, thresholds, True)
-    display_results(results)
+    if rescale_factor !=1:
+        gt_images = [gt_images[ii][::int(1/rescale_factor),::int(1/rescale_factor)] for ii in range(len(gt_images))]
+        S = rescale(S,(1,rescale_factor,rescale_factor),anti_aliasing=True)
+
+    # comment below for pantry[:12]
+    seq_len = S.shape[0]
+    gt_images = gt_images[:seq_len]
+    pred_images = [np.array(np.abs(S[ii])*255,dtype=np.uint8) for ii in range(len(S))]  # max abs val of float image is 1
+
+    # # only for pantry[:12]
+    # gt_images = gt_images[:12]
+    # pred_images = [np.array(np.abs(S[ii]) * 255, dtype=np.uint8) for ii in range(12)]  # max abs val of float image is 1
+
+
+    print('Computing full metrics...')
+    results = compute_metrics(gt_images, pred_images, thresholds)
+    display_f_score(results)
+
+    ROC_curve(results,True)
+
+
